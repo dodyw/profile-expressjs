f1a16262eb95eb3cd1e598999db6cf9f635bc18b
Dody <dodyrw@gmail.com>
2023-11-21 08:58:52 +0700

    improve json result

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,28 +9,31 @@ async function register(req, res) {
     });
 
     const { error } = schema.validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send({ success: false, message: error.details[0].message });
 
     const { name, username, profile_picture } = req.body;
 
     const user = await userModel.where('username', '==', username).get();
-    if (!user.empty) return res.status(400).send('Username already exists');
+    if (!user.empty) return res.status(400).send({ success: false, message: 'Username already exists' });
 
-    const newUser = await userModel.add({ name, username, profile_picture });
-    res.status(201).send(newUser.id);
+    const newUserRef = await userModel.add({ name, username, profile_picture });
+    const newUserDocument = await newUserRef.get();
+    const newUser = { id: newUserDocument.id, ...newUserDocument.data() };
+
+    res.status(201).send({ success: true, message: 'User registered successfully', user: newUser });
 }
 
 async function getUser(req, res) {
     const user = await userModel.doc(req.params.id).get();
-    if (!user.exists) return res.status(404).send('User not found');
-    res.send(user.data());
+    if (!user.exists) return res.status(404).send({ success: false, message: 'User not found' });
+    res.send({ success: true, message: 'User data fetched successfully', user: user.data() });
 }
 
 async function updateUser(req, res) {
     const user = await userModel.doc(req.params.id).get();
-    if (!user.exists) return res.status(404).send('User not found');
+    if (!user.exists) return res.status(404).send({ success: false, message: 'User not found' });
     await userModel.doc(req.params.id).update(req.body);
-    res.send('User updated');
+    res.send({ success: true, message: 'User updated successfully', user: req.body });
 }
 
 module.exports = {
